228b0380d7f7d6eafaf2fbdfd93714e9a5294883
Sam McAnelly <mcanelly.sam@live.com>
2014-10-10 13:30:05 -0500

    Added missing imports to fix functionality

--- RobotFrame.java
+++ RobotFrame.java
@@ -4,10 +4,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.event.ChangeListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JOptionPane;
+import javax.swing.JSlider;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
 
 /**
  *
